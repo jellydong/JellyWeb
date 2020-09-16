f1c913db274e75171541598c7580704ed3d3a575
刘家栋 <jelly.liut@gmail.com>
2020-09-16 14:39:10 +0800

    全屏的判断调整

--- src/components/Screenfull/index.vue
+++ src/components/Screenfull/index.vue
@@ -22,7 +22,8 @@ export default {
   },
   methods: {
     click() {
-      if (!screenfull.enabled) {
+      debugger
+      if (!screenfull.isEnabled) {
         this.$message({
           message: 'you browser can not work',
           type: 'warning'
@@ -35,12 +36,13 @@ export default {
       this.isFullscreen = screenfull.isFullscreen
     },
     init() {
-      if (screenfull.enabled) {
+      if (screenfull.isEnabled) {
+        debugger
         screenfull.on('change', this.change)
       }
     },
     destroy() {
-      if (screenfull.enabled) {
+      if (screenfull.isEnabled) {
         screenfull.off('change', this.change)
       }
     }
